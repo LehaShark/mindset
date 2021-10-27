2e7d22ebe62dbb8c1cbf27dad84d15eedc40fcd1
LehaShark <alexey99979@yandex.ru>
2021-10-27 17:14:33 +0300

    build for another site

--- crowler/main.py
+++ crowler/main.py
@@ -3,11 +3,12 @@ from bs4 import BeautifulSoup
 import csv
 from http.client import RemoteDisconnected
 
-TOPICS = ['/regions/oceania-region/', '/regions/central-asia/', '/regions/east-asia/', '/regions/south-asia/', '/regions/southeast-asia/', '/topics/security/', '/topics/politics/', '/topics/diplomacy/', '/topics/economy/', '/topics/society/', '/topics/environment/']
-# URL = 'https://thediplomat.com/regions/central-asia/'
 HEADERS = {'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0', 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'}
-HOST = 'https://thediplomat.com'
-FILE = 'thediplomat.csv'
+HOST = 'https://www.cbsnews.com'
+TOPICS = ['/latest/world']
+    # ['/regions/oceania-region/', '/regions/central-asia/', '/regions/east-asia/', '/regions/south-asia/', '/regions/southeast-asia/', '/topics/security/', '/topics/politics/', '/topics/diplomacy/', '/topics/economy/', '/topics/society/', '/topics/environment/']
+# URL = 'https://thediplomat.com/regions/central-asia/'
+FILE = 'cbsnews.csv'
 
 def save_file(items, path):
     with open(path, 'a', newline='') as file:
@@ -20,7 +21,11 @@ def get_text_on_page(url):
     html = get_html(url)
 
     soup = BeautifulSoup(html.text, 'html.parser')
-    text = soup.find('section', class_='td-prose').get_text()
+    try:
+        text = soup.find('section', class_='content__body').get_text(strip=True).replace(',','').replace('--', '')
+    except:
+        text = None
+        print('text is none')
     return text
 
 
@@ -30,24 +35,30 @@ def get_html(url, params=None):
 
 def get_pages_count(html):
     soup = BeautifulSoup(html, 'html.parser')
-    pagination = soup.find('div', class_='td-list-pager')
-    page = pagination.find().get_text().replace('Page 1 of ', '')
-    return int(page)
+    # pagination = soup.find('div', class_='field__item').get_text()
+    # page = pagination.find().get_text()#.replace('Page 1 of ', '')
+    return 25#int(pagination)
 
 def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
-    items = soup.find_all('a', class_='td-post')
+    items = soup.find_all('article', class_='item--topic-world')
+    # print(len(items))
     name = []
     for item in items:
-        link = str(HOST + item.get('href'))
+        #title = item.find('span', class_='field-content')
+        link = str(item.find('a', class_='item__anchor').get('href'))
+        # print(link)
         text = get_text_on_page(link)
-        name.append({
-            'text': text,
-            'title': item.find('h4').get_text().replace(',', ''),
-            'host': HOST,
-            'link': link,
-            'owner': 'mindset',
-            'label': '0.930000'
+        if text == None:
+            continue
+        else:
+            name.append({
+                'text': text,
+                'title': item.find('h4', class_='item__hed').get_text(strip=True).replace(',', ''),
+                'host': HOST,
+                'link': link,
+                'owner': 'mindset',
+                'label': '0.930000'
         })
     return name
 
@@ -58,10 +69,10 @@ def parse(url):
         pages_count = get_pages_count(html.text)
         for page in range(1, pages_count + 1):
             print(f'Парсинг страницы {page} из {pages_count}')
-            html = get_html(url + f'page/{page}/')
-            get_content(html.text)
+            html = get_html(url, params=f'page={page}')
+            # html = get_html(url + f'page/{page}/')
+            # get_content(html.text)
             articles.extend(get_content(html.text))
-            #print(articles)
         save_file(articles, FILE)
         print(f'получено {len(articles)} статей')
     else:
